75635b2ab2ae99d65b51488bcd0212c4e83ccf34
Author <43812455+Userrnam@users.noreply.github.com>
2021-08-30 21:03:33 +0300

    initial

---
+++ .cquery_cache/@Users@antonkondratuk@Desktop@cpp@ray_trace/include@stb_image.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "stb_image_read.h"
+#include "stb_image_write.h"
---
+++ build/CMakeFiles/ray_trace.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ray_trace.dir/src/main.cpp.o"
+  "CMakeFiles/ray_trace.dir/src/stb_impl.cpp.o"
+  "ray_trace"
+  "ray_trace.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ray_trace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
